aafcf9fcb9b72e2ae9601899a973daeb21357977
pachaB <eabestudio@gmail.com>
2022-10-05 19:47:58 -0300

    06- agrego colores, decoración en los logs

--- Pre-Entrega2/script.js
+++ Pre-Entrega2/script.js
@@ -8,109 +8,136 @@ const delantero = "Delantero";
 
 // JUEGO PIEDRA PAPEL O TIJERA
 function jugadaComputadora() {
-    const elementos = ["Piedra", "Papel", "Tijera"];
-    const elementoComputadora =
-      elementos[Math.floor(Math.random() * elementos.length)];
-    console.log(`Computadora eligió: ${elementoComputadora}`);
-    return elementoComputadora;
+  const elementos = ["Piedra", "Papel", "Tijera"];
+  const elementoComputadora =
+    elementos[Math.floor(Math.random() * elementos.length)];
+  console.log(`Computadora eligió: ${elementoComputadora}`);
+  return elementoComputadora;
+}
+
+function jugadaJugador() {
+  let elementoJugador;
+  let eleccionJugador = parseInt(
+    prompt(`POR FAVOR ELEGI UN VALOR :
+            1 : Piedra 
+            2 : Papel 
+            3 : Tijera`)
+  );
+  if (eleccionJugador === 1) {
+    elementoJugador = "Piedra";
+  } else if (eleccionJugador === 2) {
+    elementoJugador = "Papel";
+  } else if (eleccionJugador === 3) {
+    elementoJugador = "Tijera";
+  } else {
+    alert("Eleccion no valida");
   }
-  
-  function jugadaJugador() {
-    let elementoJugador;
-    let eleccionJugador = parseInt(
-      prompt("Ingrese un valor: 1 : Piedra 2: Papel 3: Tijera")
-    );
-    if (eleccionJugador === 1) {
-      elementoJugador = "Piedra";
-    } else if (eleccionJugador === 2) {
-      elementoJugador = "Papel";
-    } else if (eleccionJugador === 3) {
-      elementoJugador = "Tijera";
-    } else {
-      alert("Eleccion no valida");
-    }
-    console.log(`Jugador eligió: ${elementoJugador}`);
-    return elementoJugador;
-  }
-  
-  function jugarRonda(jugadorSeleccion, computadoraSeleccion) {
-    let puntajeJugador = 0;
-    let puntajeComputadora = 0;
-    // condicional para evaluar si el jugador o la compu gana la jugada
-    if (jugadorSeleccion === computadoraSeleccion) {
-      console.log(
-        `Empate --> Jugador: ${puntajeJugador} Computadora: ${puntajeComputadora}`
-      );
-    } else if (jugadorSeleccion === "Piedra" && computadoraSeleccion === "Papel") {
-      console.log(
-        `Papel le gana a Piedra--> Computadora: ${++puntajeComputadora} Jugador ${puntajeJugador}`
-      );
-    } else if (jugadorSeleccion === "Papel" && computadoraSeleccion === "Tijera") {
-      console.log(
-        `Tijera le gana a Papel--> Computadora: ${++puntajeComputadora} Jugador ${puntajeJugador}`
-      );
-    } else if (jugadorSeleccion === "Tijera" && computadoraSeleccion === "Piedra") {
-      console.log(
-        `Piedra le gana a Tijera--> Computadora: ${++puntajeComputadora} Jugador ${puntajeJugador}`
-      );
-    } else if (jugadorSeleccion === "Piedra" && computadoraSeleccion === "Tijera") {
-      console.log(
-        `Piedra le gana a Tijera--> Jugador ${++puntajeJugador} Computadora: ${puntajeComputadora}`
-      );
-    } else if (jugadorSeleccion === "Papel" && computadoraSeleccion === "Piedra") {
-      console.log(
-        `Papel le gana a Piedra-> Jugador ${++puntajeJugador} Computadora: ${puntajeComputadora}`
-      );
-    } else if (jugadorSeleccion === "Tijera" && computadoraSeleccion === "Papel") {
-      console.log(
-        `Tijera le gana a Papel-> Jugador ${++puntajeJugador} Computadora: ${puntajeComputadora}`
-      );
-    }
-    return [puntajeJugador, puntajeComputadora];
-  }
-  
-  function ganoHumano(puntajeTotalHumano) {
-    let ganador;
-    let paqueteFiguritas;
-    let ganadorHumano = puntajeTotalHumano === 3;
-    const estiloConsoleLogGanador =
-      "font-weight: bold; font-size: 14px; color: red;";
-    if (ganadorHumano) {
-      ganador = "Humano";
-      paqueteFiguritas = `\nGANASTE ${FigusPorSobre} FIGURITAS PARA LLENAR EL ÁLBUM`;
-    } else { 
-        ganador = "Computadora";
-        paqueteFiguritas = "";
-    }
+  console.log(`Jugador eligió: ${elementoJugador}`);
+  return elementoJugador;
+}
+
+function jugarRonda(jugadorSeleccion, computadoraSeleccion) {
+  let puntajeJugador = 0;
+  let puntajeComputadora = 0;
+  // condicional para evaluar si el jugador o la compu gana la jugada
+  if (jugadorSeleccion === computadoraSeleccion) {
     console.log(
-      `%cFIN DEL JUEGO - RESULTADO : Ganador ${ganador} ${paqueteFiguritas}`,
-      estiloConsoleLogGanador
+      `Empate --> Jugador: ${puntajeJugador} Computadora: ${puntajeComputadora}`
     );
-    return ganadorHumano;
+  } else if (
+    jugadorSeleccion === "Piedra" &&
+    computadoraSeleccion === "Papel"
+  ) {
+    console.log(
+      `Papel le gana a Piedra--> Computadora: ${++puntajeComputadora} Jugador ${puntajeJugador}`
+    );
+  } else if (
+    jugadorSeleccion === "Papel" &&
+    computadoraSeleccion === "Tijera"
+  ) {
+    console.log(
+      `Tijera le gana a Papel--> Computadora: ${++puntajeComputadora} Jugador ${puntajeJugador}`
+    );
+  } else if (
+    jugadorSeleccion === "Tijera" &&
+    computadoraSeleccion === "Piedra"
+  ) {
+    console.log(
+      `Piedra le gana a Tijera--> Computadora: ${++puntajeComputadora} Jugador ${puntajeJugador}`
+    );
+  } else if (
+    jugadorSeleccion === "Piedra" &&
+    computadoraSeleccion === "Tijera"
+  ) {
+    console.log(
+      `Piedra le gana a Tijera--> Jugador ${++puntajeJugador} Computadora: ${puntajeComputadora}`
+    );
+  } else if (
+    jugadorSeleccion === "Papel" &&
+    computadoraSeleccion === "Piedra"
+  ) {
+    console.log(
+      `Papel le gana a Piedra-> Jugador ${++puntajeJugador} Computadora: ${puntajeComputadora}`
+    );
+  } else if (
+    jugadorSeleccion === "Tijera" &&
+    computadoraSeleccion === "Papel"
+  ) {
+    console.log(
+      `Tijera le gana a Papel-> Jugador ${++puntajeJugador} Computadora: ${puntajeComputadora}`
+    );
   }
-  
-  function nuevaPartida() {
-    let opcion = Number(prompt("Nuevo Juego: 1 - Finalizar juego: 2"));
-    if (opcion === 1) {
-      return true;
-    } else if (opcion === 2) {
-      return false;
-    } else {
-      alert("Eleccion no valida");
-      nuevaPartida();
-    }
+  return [puntajeJugador, puntajeComputadora];
+}
+
+function ganoHumano(puntajeTotalHumano) {
+  let ganador;
+  let paqueteFiguritas;
+  let ganadorHumano = puntajeTotalHumano === 3;
+  const estiloConsoleLogGanador =
+    "font-weight: bold; font-size: 14px; color: red;";
+  if (ganadorHumano) {
+    ganador = "Humano";
+    paqueteFiguritas = `\nGANASTE ${FigusPorSobre} FIGURITAS PARA LLENAR EL ÁLBUM`;
+  } else {
+    ganador = "Computadora";
+    paqueteFiguritas = "";
   }
-  
-  function juego() {
-    pilaFigus = [];
-    
-    const estiloConsoleLogPuntaje =
-      "font-weight: bold; font-size: 12px; color: blue;";
-    
-    let continuarJuego = true;  
-    while (continuarJuego) {
-        let jugadorTotal = 0;
-        let computadoraTotal = 0;
+  console.log(
+    `%cFIN DE LA PARTIDA : Ganador ${ganador} ${paqueteFiguritas}`,
+    estiloConsoleLogGanador
+  );
+  return ganadorHumano;
+}
+
+function nuevaPartida() {
+  let opcion = Number(prompt(`
+    HOLA !!
+    Juga al piedra papel o tijera.
+    Cada partida ganada te da un sobre de figuritas.
+    Ganás el juego cuando completás el álbum.
+
+    NUEVO JUEGO: 1 - FINALIZAR JUEGO: 2`));
+  if (opcion === 1) {
+    return true;
+  } else if (opcion === 2) {
+    return false;
+  } else {
+    alert("Eleccion no valida");
+    nuevaPartida();
+  }
+}
+
+function juego() {
+  pilaFigus = [];
+
+  const estiloConsoleLogPuntaje =
+    "font-weight: bold; font-size: 12px; color: blue;";
+
+  let continuarJuego = true;
+  while (continuarJuego) {
+    let jugadorTotal = 0;
+    let computadoraTotal = 0;
     while (jugadorTotal < 3 && computadoraTotal < 3) {
       let resultado = jugarRonda(jugadaJugador(), jugadaComputadora());
       if (resultado[0] === 1) {
@@ -118,7 +145,7 @@ function jugadaComputadora() {
       } else if (resultado[1] === 1) {
         computadoraTotal += 1;
       }
-  
+
       console.log(
         `%cPuntaje jugador: ${jugadorTotal}\nPuntaje computadora: ${computadoraTotal}`,
         estiloConsoleLogPuntaje
@@ -129,7 +156,7 @@ function jugadaComputadora() {
     }
     continuarJuego = !chequeoEquipoCompleto();
     if (!continuarJuego) {
-        alert("GANASTE");
+      alert("COMPLETASTE EL ALBUM...FELICITACIONES !!!");
     }
   }
 }
@@ -169,7 +196,15 @@ equipo.push(
   defensor2,
   defensor3,
   defensor4,
-
+  defensor5,
+  mediocampista1,
+  mediocampista2,
+  mediocampista3,
+  mediocampista4,
+  delantero1,
+  delantero2,
+  delantero3,
+  delantero4
 );
 
 // métodos de Array
@@ -182,8 +217,7 @@ function generarSobre(equipo) {
   let cantFiguritas = 0;
   let figusSobre = [];
   while (cantFiguritas < FigusPorSobre) {
-    let figurita =
-      equipo[Math.floor(Math.random() * equipoMapeado.length)];
+    let figurita = equipo[Math.floor(Math.random() * equipoMapeado.length)];
     figusSobre.push(figurita);
     cantFiguritas += 1;
   }
@@ -193,22 +227,29 @@ function generarSobre(equipo) {
 }
 
 function pilaFiguritas() {
+  const estiloPila = "font-weight: bold; font-size: 12px; color: green;";
   pilaFigus = pilaFigus.concat(generarSobre(equipoMapeado));
-  console.log(pilaFigus);
-  console.log(equipoMapeado);
+  console.log(
+    `%cTENES ESTAS FIGURITAS EN TU PILA: ${pilaFigus.join(", ")}`,
+    estiloPila
+  );
 }
 
-function chequeoEquipoCompleto() { 
-    const estiloConsoleLogGanador =
-      "font-weight: bold; font-size: 14px; color: red;";
-    const chequeo = equipoMapeado.filter(x=> !pilaFigus.includes(x));
-    console.log(chequeo);
-    return chequeo.length === 0;
- 
+function chequeoEquipoCompleto() {
+  const estiloChequeo = "font-weight: bold; font-size: 12px; color: brown;";
+  const chequeo = equipoMapeado.filter((x) => !pilaFigus.includes(x));
+  if (chequeo.length !== 0) {
+    console.log(
+      `%cTE FALTAN ESTAS FIGURITAS PARA COMPLETAR EL ALBUM: ${chequeo.join(
+        ", "
+      )}`,
+      estiloChequeo
+    );
+  }
+  return chequeo.length === 0;
 }
 
 // INICIO DE JUEGO
-while(nuevaPartida()) {
-    juego();
+while (nuevaPartida()) {
+  juego();
 }
-
